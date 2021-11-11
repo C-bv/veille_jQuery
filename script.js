d48e0118d91777541f1f5469407b6bbfe78b4e6d
C-bv <charles.bouveret@outlook.fr>
2021-11-12 00:18:52 +0100

    update_11/11/2021_1

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-$("p").css("color", "#fff");
-$("p:first + p").addClass("Helloworld");
+
+
+$(document).ready(function() {
+      hideShowPageSwitcher();
+  });
 
 
 // Open/close aside + nav_toogler
@@ -8,4 +11,41 @@ $('.nav_toggler').click( function() {
       $("aside").toggleClass("open");
       $(".nav_toggler").toggleClass("open");
       $("section").toggleClass("open");
-  } );
+} );
+
+
+
+// SECTION SWITCHER
+
+const hideShowPageSwitcher = function(){
+      if ( $("section:last").hasClass("active")) {
+            $(".page_switcher_down").hide();
+          }
+      else {
+            $(".page_switcher_down").show();
+      }
+      if ( $("section:first").hasClass("active")) {
+            $(".page_switcher_up").hide();
+          }
+      else {
+            $(".page_switcher_up").show();
+      }
+}
+
+      // Switch to previous section
+      $('.page_switcher_up').click( function() {
+            $("section.active").prev().toggleClass("active");
+            $("section.active").next().removeClass("active");
+            hideShowPageSwitcher();
+      } );
+
+
+      // Switch to next section
+
+      $('.page_switcher_down').click( function() {
+            $("section.active").next().toggleClass("active");
+            $("section.active").prev().removeClass("active");
+            hideShowPageSwitcher();
+      } );
+
+// SECTION SWITCHER END
