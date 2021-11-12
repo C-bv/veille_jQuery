8ae3bb69eeabb31b574c4437aad120cdc2faee02
C-bv <charles.bouveret@outlook.fr>
2021-11-12 18:01:48 +0100

    update_12/11/2021_0

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+  
 
+// Hide loader + show start button after load
+$(document).ready(function(){
+      setTimeout(function(){
+            $(".loader").css("opacity","0");
+            $(".start").css("opacity","1");
+      },9000);
+   });
 
-$(document).ready(function() {
+// Hide preloader + page_switcher_up
+$(".start").click(function(){
+      $(".preloader").hide();
       hideShowPageSwitcher();
-  });
+});
 
 
-// Open/close aside + nav_toogler
-
-$('.nav_toggler').click( function() {
+// Open/close aside + nav_toogler = section
+$(".nav_toggler").click(function(){
       $("aside").toggleClass("open");
       $(".nav_toggler").toggleClass("open");
       $("section").toggleClass("open");
-} );
-
+});
 
 
 // SECTION SWITCHER
-
 const hideShowPageSwitcher = function(){
       if ( $("section:last").hasClass("active")) {
             $(".page_switcher_down").hide();
@@ -32,20 +39,21 @@ const hideShowPageSwitcher = function(){
       }
 }
 
-      // Switch to previous section
-      $('.page_switcher_up').click( function() {
-            $("section.active").prev().toggleClass("active");
-            $("section.active").next().removeClass("active");
-            hideShowPageSwitcher();
-      } );
+
+// Switch to previous section
+$(".page_switcher_up").click( function(){
+      $("section.active").prev().toggleClass("active");
+      $("section.active").next().removeClass("active");
+      hideShowPageSwitcher();
+});
 
 
-      // Switch to next section
-
-      $('.page_switcher_down').click( function() {
-            $("section.active").next().toggleClass("active");
-            $("section.active").prev().removeClass("active");
-            hideShowPageSwitcher();
-      } );
+// Switch to next section
+$(".page_switcher_down").click( function(){
+      $("section.active").next().toggleClass("active");
+      $("section.active").prev().removeClass("active");
+      hideShowPageSwitcher();
+});
 
 // SECTION SWITCHER END
+
