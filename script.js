59397df3a8eba0e5cd550b438e6f142259cdf8fc
C-bv <charles.bouveret@outlook.fr>
2021-11-14 01:34:14 +0100

    update_13/11/2021_0

--- script.js
+++ script.js
@@ -18,15 +18,30 @@ $(".start").click(function(){
       hideShowPageSwitcher();
 });
 
-// Open/close aside + nav_toogler = section
+// Open/close aside + nav_toogler + section
 $(".nav_toggler").click(function(){
       $("aside").toggleClass("open");
       $(".nav_toggler").toggleClass("open");
       $("section").toggleClass("open");
 });
 
+// Change nav_toogler icon + color if aside open
+let navButton = $(".nav_toggler");
+navButton.click(function(){
+      if (navButton.hasClass("open")){
+            navButton.html("<i class='fas fa-times'></i>").css("color","red");
+      }
+      else if (navButton.not("open")){
+            navButton.html("<i class='fas fa-ellipsis-h'></i>").css("color","#2e64b3");
+      }
+});
+
+// Change color of nav li a 
+
+
 
 // SECTION SWITCHER
+
 const hideShowPageSwitcher = function(){
       if ( $("section:last").hasClass("active")) {
             $(".page_switcher_down").hide();
@@ -44,32 +59,36 @@ const hideShowPageSwitcher = function(){
 
 // Switch to previous section
 $(".page_switcher_up").click( function(){
-      $("section.active").prev().toggleClass("active").removeClass("back").fadeIn();
-      $("section.active").next().removeClass("active").toggleClass("back").fadeOut();
+      $("section.active").prev().toggleClass("active").fadeIn();
+      $("section.active").next().removeClass("active").fadeOut();
       hideShowPageSwitcher();
 });
 
 
 // Switch to next section
 $(".page_switcher_down").click( function(){
-      $("section.active").next().toggleClass("active").removeClass("back").fadeIn();
-      $("section.active").prev().removeClass("active").toggleClass("back").fadeOut();
+      $("section.active").next().toggleClass("active").fadeIn();
+      $("section.active").prev().removeClass("active").fadeOut();
       hideShowPageSwitcher();
 });
 
-// let sectionNumber = 
-// function(){
 
-// }
-// if a[i] clicked the apply fn prev net to section[i]
+// Open section on navbar a click
+let navLinks = $("li > a");
+$(navLinks).click(function(){
+      let aIndex = $("a").index(this);
+      $("section:visible").removeClass("active").fadeOut();
+      $("section").eq(aIndex).toggleClass("active").fadeIn();
+      hideShowPageSwitcher();
+});
 
-$( "a" ).click(function() {
-      // `this` is the DOM element that was clicked
-      var index = $( "a" ).index( this );
-      $("section").index(2).fadeIn();
-      console.log("That was div index #" + index );
-
-    });
+// i = 1
+// $(navLinks).eq(i).click(function(){
+//       if ($(navLinks).eq(i).not("active")){
+//             $(navLinks).eq(i).toggleClass("active");
+//             $(navLinks).eq(i).siblings().removeClass("active");
+//       }
+// });
 
 
 
